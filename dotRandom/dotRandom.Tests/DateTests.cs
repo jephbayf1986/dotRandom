839644c77b62f66b3b0191374b0e45111b820864
Jeph Bayfield <jeph.bayfield@bcn.co.uk>
2022-09-27 14:39:51 +0100

    done testing on dates

---
+++ dotRandom/dotRandom.Tests/DateTests.cs
@@ -0,0 +1,144 @@
+﻿namespace dotRandom.Tests
+{
+    public class DateTests
+    {
+        private const int numberOfTests = 1000;
+
+        [Fact]
+        public void DateInPastMustBeInPast()
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInPast());
+
+            // Assert
+            resultList.All(x => x < DateTime.Today)
+                      .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void DateInPastWhenDefaultMustBeWithin100Days()
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInPast());
+
+            // Assert
+            resultList.All(x => x >= DateTime.Today.AddDays(-100))
+                      .ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void DateInPastMustBeWithinRange(int maxDaysInPast)
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInPast(maxDaysInPast));
+
+            // Assert
+            resultList.All(x => x >= DateTime.Today.AddDays(0 - maxDaysInPast))
+                      .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void DateInPastWith1DayRangeMustBeYesterday()
+        {
+            // Arrange
+
+            // Act
+            var result = DotRandom.RandomDateInPast(1);
+
+            // Assert
+            result.Date.ShouldBe(DateTime.Today.AddDays(-1));
+        }
+
+        [Fact]
+        public void DateInFutureMustBeInFuture()
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInFuture());
+
+            // Assert
+            resultList.All(x => x >= DateTime.Today.AddDays(1))
+                      .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void DateInFutureWhenDefaultMustBeWithin100Days()
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInFuture());
+
+            // Assert
+            resultList.All(x => x < DateTime.Today.AddDays(100))
+                      .ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void DateInFutureMustBeWithinRangePlus1(int maxDaysInFuture)
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInFuture(maxDaysInFuture));
+
+            // Assert
+            resultList.All(x => x < DateTime.Today.AddDays(maxDaysInFuture + 1))
+                      .ShouldBeTrue();
+        }
+
+        [Fact]
+        public void DateInFutureWith1DayRangeMustBeTomorrow()
+        {
+            // Arrange
+
+            // Act
+            var result = DotRandom.RandomDateInFuture(1);
+
+            // Assert
+            result.Date.ShouldBe(DateTime.Today.AddDays(1));
+        }
+
+        [Theory]
+        [InlineData(2020)]
+        [InlineData(2022)]
+        public void DateInYearMustBeWithinYear(int year)
+        {
+            // Arrange
+            var resultList = new List<DateTime>();
+
+            // Act
+            for (int i = 0; i < numberOfTests; i++)
+                resultList.Add(DotRandom.RandomDateInYear(year));
+
+            // Assert
+            resultList.All(x => x.Year == year)
+                      .ShouldBeTrue();
+        }
+    }
+}
--- dotRandom/dotRandom/Date.cs
+++ dotRandom/dotRandom/Date.cs
@@ -4,22 +4,63 @@ namespace dotRandom
 {
     public static partial class DotRandom
     {
+        /// <summary>
+        /// Random Date in the Past
+        /// </summary>
+        /// <param name="maxDaysInPast">Maximum days to go back (Default 100 days)</param>
+        /// <returns>Date Time from past</returns>
         public static DateTime RandomDateInPast(int maxDaysInPast = 100)
         {
             var randGen = new Random();
 
             var daysInPast = randGen.Next(1, maxDaysInPast);
 
-            return DateTime.Now.AddDays(0 - daysInPast);
+            return DateTime.Today.AddDays(0 - daysInPast)
+                                 .WithRandomTime();
         }
 
+        /// <summary>
+        /// Random Date in the Future
+        /// </summary>
+        /// <param name="maxDaysInPast">Maximum days to go forward (Default 100 days)</param>
+        /// <returns>Date Time from future</returns>
         public static DateTime RandomDateInFuture(int maxDaysInFuture = 100)
         {
             var randGen = new Random();
 
-            var daysInPast = randGen.Next(1, maxDaysInFuture);
+            var daysInFuture = randGen.Next(1, maxDaysInFuture);
 
-            return DateTime.Now.AddDays(daysInPast);
+            return DateTime.Today.AddDays(daysInFuture)
+                                 .WithRandomTime();
+        }
+
+        /// <summary>
+        /// Random Date in a Year
+        /// </summary>
+        /// <param name="year">Year to pick a date from</param>
+        /// <returns>Date Time within Year</returns>
+        public static DateTime RandomDateInYear(int year)
+        {
+            var randGen = new Random();
+
+            var daysInYear = DateTime.IsLeapYear(year) ? 366 : 365;
+
+            var randomDayInYear = randGen.Next(0, daysInYear);
+
+            return new DateTime(year, 1, 1).AddDays(randomDayInYear)
+                                           .WithRandomTime();
+        }
+
+        /// <summary>
+        /// Same Date with Random Time
+        /// </summary>
+        /// <param name="dateTime">Date Time to change the time on</param>
+        /// <returns>Date Time with different Time from input</returns>
+        public static DateTime WithRandomTime(this DateTime dateTime)
+        {
+            return dateTime.Date.AddHours(RandomIntBetween(0, 23))
+                                .AddMinutes(RandomIntBetween(0, 59))
+                                .AddSeconds(RandomIntBetween(0, 59));
         }
     }
 }
